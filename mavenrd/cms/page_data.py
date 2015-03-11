95ba66baf6b03794fe44136bf8446b69ee9c34dd
Matt ODonnell <matt.odonnell@leandog.com>
2015-03-10 22:03:14 -0400

    order blog post by descending order

--- mavenrd/cms/page_data.py
+++ mavenrd/cms/page_data.py
@@ -1,4 +1,4 @@
 from cms.models import Post
 from common.forms import ContactForm
 def blog():
-    return {"posts": Post.objects.filter(active=True) }
+    return {"posts": Post.objects.filter(active=True).order_by('-created_at') }
